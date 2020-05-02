3799b007dc82cbeb182c68b81e93c4e29e3809ac
oshan2csd <oshan2csd@gmail.com>
2020-05-02 11:37:27 +1000

    adding the project to git

---
+++ src/main/java/oshan2csd/springbootmongodbdemo/SpringbootMongodbDemoApplication.java
@@ -0,0 +1,13 @@
+package oshan2csd.springbootmongodbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oshan2csd/springbootmongodbdemo/SpringbootMongodbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package oshan2csd.springbootmongodbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
